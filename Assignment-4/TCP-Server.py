c7b04f9d71ceb19d81636721e1f13c5679716c74
Blake <blake.perrin13@gmail.com>
2024-04-24 11:45:04 -0500

    Updated

--- Assignment-4/TCP-Server.py
+++ Assignment-4/TCP-Server.py
@@ -6,14 +6,22 @@ server_socket.listen(1)
 print("Server is ready to receive...\n")
 
 while(True):
-    connection_socket, address = server_socket.accept()
-    incoming_message = connection_socket.recv(2048)
+    try: 
+        connection_socket, address = server_socket.accept()
+        incoming_message = connection_socket.recv(2048)
 
-    print("Data: ")
-    print(incoming_message.decode())
+        print("Client Connected...")
+        print("Client Address: ", address[0])
+        print("Message Received: " + incoming_message.decode())
+        print()
 
-    return_message = "Message Received..."
+        return_message = "Message Received..."
 
 
-    connection_socket.send(return_message.encode())
-    connection_socket.close()
+        connection_socket.send(return_message.encode())
+        connection_socket.close()
+
+    except KeyboardInterrupt:
+        print("\nServer is shutting down...")
+        server_socket.close()
+        break
