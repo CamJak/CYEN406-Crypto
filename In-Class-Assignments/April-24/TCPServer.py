a014a6c1524fe9b3196c4ae4883e71df3d547eca
J4keFr0mStatef <80546415+J4keFr0mStatef@users.noreply.github.com>
2024-04-24 11:30:37 -0500

    Created Client and Server code for TCP

---
+++ In-Class-Assignments/April-24/TCPServer.py
@@ -0,0 +1,48 @@
+# Travis Knippers
+# Assignment 2
+
+from socket import *
+
+server_socket = socket(AF_INET, SOCK_STREAM) # SOCK_STREAM refers to TCP-type connection
+server_socket.bind(('', 12000)) # (IP, Port No.)
+server_socket.listen(1) # infinitely listen
+print("The server is ready to receive...\n")
+
+host = gethostname()
+
+while(True):
+    socket_cxn, address = server_socket.accept()
+    request_cxn = socket_cxn.recv(2048)
+    
+    req_decoded = request_cxn.decode()
+
+    print(f"HTTP Request:\n{req_decoded}")
+    print(f"Host: {gethostbyname(host)}\n")
+
+    #parse request
+    parsed = req_decoded.split()
+    mssg = parsed[0]
+    path = parsed[1]
+    version = parsed[2]
+
+    path_parsed = path.split("/")
+    obj = path_parsed[-1]    
+    print(f"Object to be fetched: {obj}")
+    # attempt to open and read the contents of obj if it exists
+    try:
+        obj_cont = open(obj)
+        content = obj_cont.read()
+        print(f"Object content:\n{content}\n")
+        code = 200
+        resp_mssg = "OK"
+
+    except:
+        code = 404
+        resp_mssg = "Not Found"    
+
+    response = f"{version} {code} {resp_mssg}"
+    if code != 404:
+        response += "\n\n" + str(content)
+    print(f"HTTP response message:\n{response}\n")
+
+    socket_cxn.send(response.encode())
